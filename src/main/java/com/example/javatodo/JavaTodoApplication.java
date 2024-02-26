985b39c38bbc29b46463bc69d715c72b363579ab
jinic808 <kimjini9030@gmail.com>
2024-02-25 20:10:36 -0700

    Added dependencies

---
+++ src/main/java/com/example/javatodo/JavaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javatodo/JavaTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
